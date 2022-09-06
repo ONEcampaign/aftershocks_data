bc91b6bd9707b85abf67cc0b560d1692899efefb
Jorge Rivera <jorge.rivera@one.org>
2022-09-06 11:05:21 +0200

    chart scripts

--- scripts/oda/topic_page.py
+++ scripts/oda/topic_page.py
@@ -1,7 +1,140 @@
 from scripts.config import PATHS
 import pandas as pd
+from pydeflate import deflate
+from bblocks.cleaning_tools.clean import format_number, convert_id
+from bblocks.cleaning_tools.filter import filter_latest_by
+
+DAC = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    18,
+    20,
+    21,
+    22,
+    40,
+    50,
+    61,
+    68,
+    69,
+    75,
+    76,
+    301,
+    302,
+    701,
+    742,
+    801,
+    820,
+    918,
+]
 
 
+def _read_total_oda(official_definition: bool = True) -> pd.DataFrame:
+    """Read the csv containing total ODA data"""
+    flow = pd.read_csv(f"{PATHS.raw_oda}/total_oda_flow.csv", parse_dates=["year"])
+    ge = pd.read_csv(f"{PATHS.raw_oda}/total_oda_ge.csv", parse_dates=["year"])
+
+    if official_definition:
+        flow = flow.loc[flow.year.dt.year < 2018]
+        ge = ge.loc[ge.year.dt.year >= 2018]
+
+    return pd.concat([flow, ge], ignore_index=True).loc[
+        lambda d: d.donor_code.isin(DAC)
+    ]
 
 
+def _append_DAC_total(df: pd.DataFrame) -> pd.DataFrame:
+    """Append the "DAC Countries, Total" value to the dataframe.
+    Identify with code 20001"""
 
+    df_dac = (
+        df.loc[lambda d: d.donor_code != 918]
+        .groupby(["year", "flows_code", "indicator"], as_index=False)
+        .sum()
+        .assign(donor_code=20001)
+    )
+
+    return pd.concat([df, df_dac], ignore_index=True)
+
+
+def _add_constant_change_column(df: pd.DataFrame, base: int) -> pd.DataFrame:
+    """Add a column with the change in constant terms"""
+
+    df_constant = deflate(
+        df,
+        base_year=base,
+        date_column="year",
+        source="oecd_dac",
+        id_column="donor_code",
+        id_type="DAC",
+        target_col="value_constant",
+    )
+
+    df_constant["pct_change"] = df_constant.groupby(["donor_code", "flows_code"])[
+        "value_constant"
+    ].pct_change()
+
+    df_constant["pct_change"] = format_number(
+        df_constant["pct_change"],
+        as_percentage=True,
+    ).replace("nan%", "")
+
+    return df_constant
+
+
+def global_aid_key_number() -> None:
+    """Create an overview chart which contains the latest total ODA value and
+    the change in constant terms."""
+
+    df = (
+        _read_total_oda(official_definition=True)
+        .pipe(_append_DAC_total)
+        .pipe(_add_constant_change_column, base=2021)
+        .assign(
+            name=lambda d: convert_id(
+                d.donor_code,
+                from_type="DACcode",
+                to_type="name_short",
+                additional_mapping={
+                    20001: "DAC Countries, Total",
+                    918: "EU Institutions",
+                },
+            ),
+            year=lambda d: d.year.dt.year,
+        )
+        .pipe(
+            filter_latest_by,
+            date_column="year",
+            group_by=["name"],
+            value_columns=["value", "pct_change"],
+        )
+        .filter(["name", "year", "value", "pct_change"], axis=1)
+        .rename(
+            columns={
+                "value": "Total ODA (USD million)",
+                "pct_change": "Real year-on-year change",
+            }
+        )
+    )
+
+    # chart version
+    df.to_csv(f"{PATHS.charts}/oda_topic/key_number_total_oda.csv", index=False)
+
+    # download version
+    source = "OECD DAC Creditor Reporting System (CRS)"
+    df.assign(source=source).to_csv(
+        f"{PATHS.download}/oda_topic/key_number_total_oda.csv", index=False
+    )
+
+
+if __name__ == "__main__":
+    global_aid_key_number()
