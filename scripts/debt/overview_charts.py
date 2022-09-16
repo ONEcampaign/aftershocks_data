0b121f34d739575ffaea8aaf4efb58b10b7db641
Jorge Rivera <jorge.rivera@one.org>
2022-09-16 18:02:44 +0100

    debt key numbers

--- scripts/debt/overview_charts.py
+++ scripts/debt/overview_charts.py
@@ -1,6 +1,9 @@
 import datetime
 
 from scripts.debt.common import read_dservice_data, read_dstocks_data
+from bblocks.import_tools.debt.common import get_dsa
+from bblocks.cleaning_tools.clean import convert_id
+
 import pandas as pd
 from scripts.config import PATHS
 from bblocks.cleaning_tools.clean import format_number
@@ -8,6 +11,23 @@ from bblocks.cleaning_tools.clean import format_number
 KEY_NUMBERS: dict = {}
 
 CURRENT_YEAR = datetime.datetime.now().year
+
+
+def debt_distress() -> None:
+    df = get_dsa(update=False, local_path=f"{PATHS.raw_data}/dsa_list.pdf")
+
+    df = df.assign(continent=lambda d: convert_id(d.country, to_type="continent")).loc[
+        lambda d: d.risk_of_debt_distress.isin(["High", "In debt distress"])
+    ]
+
+    africa = df.loc[df.continent == "Africa", :]
+
+    number = len(africa)
+
+    KEY_NUMBERS["debt_distress_africa_share"] = (
+        str(round(100 * len(africa) / len(df))) + "%"
+    )
+    KEY_NUMBERS["debt_distress_africa"] = str(number)
 
 
 def debt_service_africa_trend() -> None:
@@ -27,7 +47,7 @@ def debt_service_africa_trend() -> None:
         + " billion"
     )
 
-    df.to_csv(f"{PATHS.charts}/debt/debt_service_africa_trend.csv", index=False)
+    df.to_csv(f"{PATHS.charts}/debt_topic/debt_service_africa_trend.csv", index=False)
 
 
 def debt_stocks_africa_trend() -> None:
@@ -46,9 +66,21 @@ def debt_stocks_africa_trend() -> None:
         + " billion"
     )
 
-    df.to_csv(f"{PATHS.charts}/debt/debt_stocks_africa_trend.csv", index=False)
+    df.to_csv(f"{PATHS.charts}/debt_topic/debt_stocks_africa_trend.csv", index=False)
+
+
+def export_key_numbers_overview() -> None:
+    """Export KEY_NUMBERS dictionary as json"""
+    import json
+
+    with open(f"{PATHS.charts}/debt_topic/key_numbers.json", "w") as f:
+        json.dump(KEY_NUMBERS, f, indent=4)
 
 
 debt_service_africa_trend()
 
 debt_stocks_africa_trend()
+
+debt_distress()
+
+export_key_numbers_overview()
