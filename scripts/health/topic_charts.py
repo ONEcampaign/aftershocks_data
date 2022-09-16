be498cb9142cf00be3bf6701fb4d8aabb6ea4441
lpicci96 <lpicci96@gmail.com>
2022-09-16 12:17:57 +0200

    add malaria

--- scripts/health/topic_charts.py
+++ scripts/health/topic_charts.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from scripts.config import PATHS
+import requests
 
 
 def hiv_topic_chart():
@@ -30,8 +31,33 @@ def hiv_topic_chart():
     df.to_csv(f'{PATHS.download}/health/hiv_topic_chart.csv', index=False)
 
 
+def malaria_topic_chart():
+    """ """
+
+    #to be replaced with bblocks then database
+    URL = 'https://ghoapi.azureedge.net/api/'
+    code = 'MALARIA_EST_DEATHS'
+
+    url_malaria = URL + code
+
+    request = requests.get(url_malaria)
+    data = request.json()
+    df = pd.DataFrame.from_records(data['value'])
+
+    df = (df.loc[df.SpatialDim.isin(['GLOBAL', 'AFR']), ['SpatialDim', 'TimeDim', 'NumericValue']]
+          .pivot(index='TimeDim', columns = 'SpatialDim', values = 'NumericValue')
+          .reset_index()
+          .assign(rest = lambda d: d['GLOBAL'] - d['AFR'])
+          .rename(columns = {'TimeDim': 'year', 'AFR': 'Africa', 'GLOBAL': 'Global', 'rest': 'Rest of the world'})
+
+          )
+
+    df.to_csv(f'{PATHS.charts}/health/malaria_topic_chart.csv', index=False)
+    df.to_csv(f'{PATHS.download}/health/malaria_topic_chart.csv', index=False)
+
 
 
 
 if __name__ == "__main__":
     hiv_topic_chart()
+    malaria_topic_chart()
