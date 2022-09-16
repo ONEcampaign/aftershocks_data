ab32ce8dab487ba9cb3bca8da9bd0689ab2c1bc3
lpicci96 <lpicci96@gmail.com>
2022-09-16 13:47:27 +0200

    add DTP charts

--- scripts/health/topic_charts.py
+++ scripts/health/topic_charts.py
@@ -12,17 +12,18 @@ def hiv_topic_chart():
     treatment = pd.read_csv(f'{PATHS.raw_data}/health/aids_region_People living with HIV receiving ART (%).csv')
 
     regions = {'UNAAP': 'Asia and the Pacific', 'UNACAR': 'Caribbean', 'UNAESA': 'East and Southern Africa',
-               'UNAEECA': 'Eastern Europe and Central Asia', 'UNALA': 'Latin America', 'UNAMENA': 'Middle East and North Africa',
-               'UNAWCA': 'West and Central Africa', 'UNAWCENA':'Western & Central Europe and North America'
+               'UNAEECA': 'Eastern Europe and Central Asia', 'UNALA': 'Latin America',
+               'UNAMENA': 'Middle East and North Africa',
+               'UNAWCA': 'West and Central Africa', 'UNAWCENA': 'Western & Central Europe and North America'
 
                }
 
-    deaths = deaths.assign(indicator = 'AIDS-related deaths')
-    treatment = treatment.assign(indicator = 'People living with HIV receiving ART')
+    deaths = deaths.assign(indicator='AIDS-related deaths')
+    treatment = treatment.assign(indicator='People living with HIV receiving ART')
 
     df = (pd.concat([deaths, treatment])
-          .assign(region = lambda d: d['country'].map(regions))
-          .pivot(index=['region', 'year'], columns = 'indicator', values='All ages estimate')
+          .assign(region=lambda d: d['country'].map(regions))
+          .pivot(index=['region', 'year'], columns='indicator', values='All ages estimate')
           .dropna(subset=['People living with HIV receiving ART', 'AIDS-related deaths'])
           .reset_index()
           )
@@ -31,24 +32,30 @@ def hiv_topic_chart():
     df.to_csv(f'{PATHS.download}/health/hiv_topic_chart.csv', index=False)
 
 
-def malaria_topic_chart():
-    """ """
+def query_who(code: str):
+    """To be replaced in bblocks"""
 
-    #to be replaced with bblocks then database
     URL = 'https://ghoapi.azureedge.net/api/'
-    code = 'MALARIA_EST_DEATHS'
 
-    url_malaria = URL + code
-
-    request = requests.get(url_malaria)
+    request = requests.get(URL + code)
     data = request.json()
     df = pd.DataFrame.from_records(data['value'])
 
+    return df
+
+
+def malaria_topic_chart():
+    """ """
+
+    code = 'MALARIA_EST_DEATHS'
+
+    df = query_who(code)
+
     df = (df.loc[df.SpatialDim.isin(['GLOBAL', 'AFR']), ['SpatialDim', 'TimeDim', 'NumericValue']]
-          .pivot(index='TimeDim', columns = 'SpatialDim', values = 'NumericValue')
+          .pivot(index='TimeDim', columns='SpatialDim', values='NumericValue')
           .reset_index()
-          .assign(rest = lambda d: d['GLOBAL'] - d['AFR'])
-          .rename(columns = {'TimeDim': 'year', 'AFR': 'Africa', 'GLOBAL': 'Global', 'rest': 'Rest of the world'})
+          .assign(rest=lambda d: d['GLOBAL'] - d['AFR'])
+          .rename(columns={'TimeDim': 'year', 'AFR': 'Africa', 'GLOBAL': 'Global', 'rest': 'Rest of the world'})
 
           )
 
@@ -56,8 +63,26 @@ def malaria_topic_chart():
     df.to_csv(f'{PATHS.download}/health/malaria_topic_chart.csv', index=False)
 
 
+def dtp_topic_chart():
+    """ """
+
+    code = 'WHS4_100'
+    df = query_who(code)
+
+    df = (df.loc[df.SpatialDimType == 'WORLDBANKINCOMEGROUP', ['SpatialDim', 'TimeDim', 'NumericValue']]
+          .pivot(index='TimeDim', columns='SpatialDim', values='NumericValue')
+          .reset_index()
+          .rename(columns={'TimeDim': 'year', 'WB_HI': 'High-income', 'WB_LI': 'Low-income',
+                           'WB_LMI': 'Lower-middle-income', 'WB_UMI': 'Upper-middle-income'})
+          )
+
+    df.to_csv(f'{PATHS.charts}/health/DTP_topic_chart.csv', index=False)
+    df.to_csv(f'{PATHS.download}/health/DTP_topic_chart.csv', index=False)
 
 
 if __name__ == "__main__":
-    hiv_topic_chart()
-    malaria_topic_chart()
+
+# hiv_topic_chart()
+# malaria_topic_chart()
+    dtp_topic_chart() 
+
