60d71bb6f5777a64937ba6c4cfd296a796face68
lpicci96 <lpicci96@gmail.com>
2022-09-19 11:19:46 +0200

    add overview charts

--- scripts/health/overview_charts.py
+++ scripts/health/overview_charts.py
@@ -2,14 +2,34 @@
 
 from bblocks.import_tools.world_bank import WorldBankData
 import pandas as pd
+from scripts.config import PATHS
 
-indicators = {'Life expectancy at birth, total (years)': 'SP.DYN.LE00.IN',
-              'Mortality rate, under-5 (per 1,000 live births)': 'SH.DYN.MORT',
-              
+chart_indicators = {'life_expectancy_overview': 'SP.DYN.LE00.IN',
+                    'infant_mortality_overview': 'SH.DYN.MORT',
+                    'health_expenditure_overview': 'SH.XPD.CHEX.GD.ZS',
+                    }
 
-              }
+
+def __clean_overview_chart(df: pd.DataFrame) -> pd.DataFrame:
+    """Create overview line chart with SSA and WLD data"""
+
+    return (df
+            .loc[lambda d: d['iso_code'].isin(['WLD', 'SSA'])]
+            .pivot(index='date', columns='iso_code', values='value')
+            .reset_index()
+            .dropna(subset=['SSA', 'WLD'])
+            .rename(columns={'SSA': 'Sub-Saharan Africa', 'WLD': 'World'})
+            )
 
 
-wb = WorldBankData(update_data=True)
-for _, code in indicators.items():
-    wb.load_indicator(code)
+def create_overview_charts():
+    """Create overview charts"""
+
+    wb = WorldBankData(update_data=True)
+    for _, code in chart_indicators.items():
+        wb.load_indicator(code)
+
+    for name, code in chart_indicators.items():
+        (wb.get_data(code)
+         .pipe(__clean_overview_chart)
+         .to_csv(f'{PATHS.charts}/health/{name}.csv', index=False))
