8f332c4d836d221f9a0f3bc7e6504c5454fc2e49
Jorge Rivera <jorge.rivera@one.org>
2022-09-21 15:50:42 +0200

    base explorer

--- scripts/explorers/data.py
+++ scripts/explorers/data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import country_converter
-from bblocks.dataframe_tools.add import add_income_level_column
+from bblocks.dataframe_tools import add
+
+from scripts.explorers.common import LDC, ExplorerSchema
 
 
 def _base_df() -> pd.DataFrame:
@@ -14,15 +16,36 @@ def _base_df() -> pd.DataFrame:
 def basic_info() -> pd.DataFrame:
     """Create a DataFrame with basic information"""
 
-    base = (
+    return (
         _base_df()
-        .pipe(add_income_level_column, id_column="ISO3", id_type="ISO3")
-        .fillna({"income_level": "Not classified"})
+        .pipe(add.add_income_level_column, id_column="ISO3", id_type="ISO3")
+        .pipe(add.add_population_column, id_column="ISO3", id_type="ISO3")
+        .pipe(
+            add.add_gdp_column,
+            id_column="ISO3",
+            id_type="ISO3",
+            usd=True,
+            include_estimates=False,
+        )
+        .pipe(add.add_poverty_ratio_column, id_column="ISO3", id_type="ISO3")
+        .assign(ldc=lambda d: d.ISO3.apply(lambda x: "LDC" if x in LDC else "Non-LDC"))
+        .assign(gdp_per_capita=lambda d: d.gdp / d.population)
+        .drop("gdp", axis=1)
+        .dropna(thresh=6)
+        .rename(
+            columns={
+                "ISO3": ExplorerSchema.ID,
+                "name_short": ExplorerSchema.NAME,
+                "UNregion": ExplorerSchema.REGION,
+                "continent": ExplorerSchema.CONTINENT,
+                "income_level": ExplorerSchema.INCOME,
+                "population": ExplorerSchema.POP,
+                "poverty_ratio": ExplorerSchema.POVERTY,
+                "ldc": ExplorerSchema.LDC,
+                "gdp_per_capita": ExplorerSchema.GDP,
+            }
+
+        )
     )
 
-import requests
-url = requests.get("http://unctad.org/topic/least-developed-countries/list")
 
-
-
-df = pd.read_html(url.text)
