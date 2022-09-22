50b4bc5531758c56a66982aae8127625029fab55
Jorge Rivera <jorge.rivera@one.org>
2022-09-22 11:05:31 +0200

    econ explorer data

--- scripts/explorers/common.py
+++ scripts/explorers/common.py
@@ -1,5 +1,10 @@
 import country_converter
-from bblocks.dataframe_tools.add import add_flourish_geometries
+import pandas as pd
+from bblocks.dataframe_tools.add import add_flourish_geometries, add_short_names_column
+from bblocks.dataframe_tools.common import get_poverty_ratio_df, get_population_df
+from bblocks.import_tools.imf import WorldEconomicOutlook
+
+WEO_YEAR = 2022
 
 
 class ExplorerSchema:
@@ -9,9 +14,13 @@ class ExplorerSchema:
     CONTINENT = "Continent"
     INCOME = "Income Group"
     LDC = "Least Developed Countries"
-    GDP = "GDP per capita"
+    GDP = "GDP per capita (USD)"
     POP = "Population"
-    POVERTY = "Poverty Headcount Ratio"
+    POVERTY = "Poverty rate (%)"
+    GDP_GROWTH = "GDP growth (%)"
+    UNEMPLOYMENT = "Unemployment rate (%)"
+    GOV_REVENUE = "General Government Revenue (% of GDP)"
+    GOV_EXPENDITURE = "General Government Expenditure (% of GDP)"
 
 
 def base_africa_map():
@@ -76,3 +85,65 @@ LDC: list = [
     "YEM",
     "ZMB",
 ]
+
+
+ECONOMICS_INDICATORS = {
+    "NGDP_RPCH": ExplorerSchema.GDP_GROWTH,
+    "LUR": ExplorerSchema.UNEMPLOYMENT,
+    "GGR_NGDP": ExplorerSchema.GOV_REVENUE,
+    "GGX_NGDP": ExplorerSchema.GOV_EXPENDITURE,
+}
+
+
+def _weo_meta() -> pd.DataFrame:
+    weo = WorldEconomicOutlook()
+
+    for indicator in ECONOMICS_INDICATORS:
+        weo.load_indicator(indicator)
+
+    return (
+        weo.get_data(keep_metadata=True)
+        .loc[lambda d: d.year.dt.year == WEO_YEAR]
+        .assign(
+            indicator=lambda d: d.indicator.map(ECONOMICS_INDICATORS),
+            year=lambda d: d.year.dt.year,
+        )
+        .filter(["iso_code", "year", "indicator", "estimate"], axis=1)
+        .assign(source="IMF World Economic Outlook")
+    )
+
+
+def _wb_meta() -> pd.DataFrame:
+
+    # population
+    population = (
+        get_population_df(most_recent_only=True, update_population_data=False)
+        .filter(["iso_code", "year"])
+        .assign(indicator=ExplorerSchema.POP, source="World Bank Open Data")
+    )
+
+    # poverty
+    poverty = (
+        get_poverty_ratio_df(most_recent_only=True, update_poverty_data=False)
+        .filter(["iso_code", "year", "poverty_ratio"])
+        .assign(indicator=ExplorerSchema.POVERTY, source="World Bank Open Data")
+    )
+
+    return pd.concat([population, poverty], ignore_index=True)
+
+
+def indicators_metadata():
+    """DataFrame with information on the indicators used by the explorers"""
+
+    df_weo = _weo_meta()
+    df_wb = _wb_meta()
+
+    df = (
+        pd.concat([df_weo, df_wb], ignore_index=True)
+        .pipe(add_short_names_column, id_column="iso_code", id_type="ISO3")
+        .rename(
+            columns={"short_name": ExplorerSchema.NAME, "iso_code": ExplorerSchema.ID}
+        )
+    )
+
+    return df
--- scripts/explorers/data.py
+++ scripts/explorers/data.py
@@ -1,8 +1,16 @@
-import pandas as pd
 import country_converter
+import pandas as pd
 from bblocks.dataframe_tools import add
+from bblocks.import_tools.imf import WorldEconomicOutlook
 
-from scripts.explorers.common import LDC, ExplorerSchema
+from scripts.config import PATHS
+from scripts.explorers.common import (
+    LDC,
+    ExplorerSchema,
+    WEO_YEAR,
+    ECONOMICS_INDICATORS,
+    indicators_metadata,
+)
 
 
 def _base_df() -> pd.DataFrame:
@@ -32,6 +40,7 @@ def basic_info() -> pd.DataFrame:
         .assign(gdp_per_capita=lambda d: d.gdp / d.population)
         .drop("gdp", axis=1)
         .dropna(thresh=6)
+        .fillna({"income_level": "Not classified"})
         .rename(
             columns={
                 "ISO3": ExplorerSchema.ID,
@@ -44,8 +53,44 @@ def basic_info() -> pd.DataFrame:
                 "ldc": ExplorerSchema.LDC,
                 "gdp_per_capita": ExplorerSchema.GDP,
             }
-
         )
     )
 
 
+def _base_weo_economics() -> pd.DataFrame:
+    weo = WorldEconomicOutlook()
+
+    for indicator in ECONOMICS_INDICATORS:
+        weo.load_indicator(indicator)
+
+    return (
+        weo.get_data(keep_metadata=False)
+        .loc[lambda d: d.year.dt.year == WEO_YEAR]
+        .assign(
+            indicator=lambda d: d.indicator.map(ECONOMICS_INDICATORS),
+            year=lambda d: d.year.dt.year,
+        )
+        .pivot(index=["iso_code", "year"], columns="indicator", values="value")
+        .reset_index()
+        .drop("year", axis=1)
+        .rename(columns={"iso_code": ExplorerSchema.ID})
+    )
+
+
+def econ_explorer():
+    base = basic_info()
+    econ = _base_weo_economics()
+
+    df = base.merge(econ, on=ExplorerSchema.ID, how="left")
+
+    metadata = indicators_metadata().loc[lambda d: d.indicator.isin(df.columns)]
+
+    # Export explorer
+    df.to_csv(f"{PATHS.charts}/explorers/economics.csv", index=False)
+
+    # Export metadata
+    metadata.to_excel(f"{PATHS.download}/explorers/econ_metadata.xlsx", index=False)
+
+
+if __name__ == "__main__":
+    econ_explorer()
