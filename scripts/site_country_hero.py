46ef9832f66b99d0b817db47d738bb22e6256d93
Jorge Rivera <jorge.rivera@one.org>
2022-09-23 14:50:50 +0200

    inflation indicator change

--- scripts/site_country_hero.py
+++ scripts/site_country_hero.py
@@ -52,7 +52,7 @@ def _group_monthly_change(
     percentage: bool,
     months: int = 1,
 ) -> pd.DataFrame:
-    """""" ""
+
     sdate = group.date.max() - relativedelta(months=months)
     edate = group.date.max()
 
@@ -92,6 +92,7 @@ def _wfp_charts() -> None:
         .loc[lambda d: d.date.dt.year.between(2018, 2022)]
         .loc[lambda d: d.indicator == "Inflation Rate"]
         .filter(["name_short", "date", "indicator", "value"], axis=1)
+        .rename(columns={"indicator": "indicator_name"})
     )
 
     # Chart version
