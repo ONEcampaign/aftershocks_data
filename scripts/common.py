692c2a56cb631d9cee3ef9c20d3873ec7dd2668e
lpicci96 <lpicci96@gmail.com>
2022-09-28 11:06:45 +0200

    Update common.py

--- scripts/common.py
+++ scripts/common.py
@@ -50,3 +50,18 @@ def base_africa_df():
 
 
 WEO_YEAR: int = 2022
+
+
+def clean_wb_overview(df: pd.DataFrame) -> pd.DataFrame:
+    """Clean World Bank data for overview charts
+
+    returns a dataframe for a line chart with values for World and SSA
+    """
+
+    return (df
+            .loc[lambda d: d['iso_code'].isin(['WLD', 'SSA'])]
+            .pivot(index='date', columns='iso_code', values='value')
+            .reset_index()
+            .dropna(subset=['SSA', 'WLD'])
+            .rename(columns={'SSA': 'Sub-Saharan Africa', 'WLD': 'World'})
+            )
