edd60129691b0f7e8c0935997b7dffdff88c3a0a
lpicci96 <lpicci96@gmail.com>
2022-09-28 10:56:38 +0200

    Squashed commit of the following:
    commit 3abc87b80f2ba8064521a77827db71bdbefd445a
    Author: Jorge Rivera <jorge.rivera@one.org>
    Date:   Tue Sep 27 18:25:21 2022 +0200
    
        debt sm
    
    commit f1c013c9cf787546c29c30cb2106a6bd5412585c
    Author: Jorge Rivera <jorge.rivera@one.org>
    Date:   Tue Sep 27 17:59:07 2022 +0200
    
        vax
    
    commit bf8a3dae7a89a5e574a030523299fb50887996ce
    Author: Jorge Rivera <jorge.rivera@one.org>
    Date:   Tue Sep 27 15:50:43 2022 +0200
    
        overview charts
    
    commit c2634010b1ab0e7b0803cd8b0e8badc404a98467
    Author: Jorge Rivera <jorge.rivera@one.org>
    Date:   Sat Sep 24 00:42:09 2022 +0200
    
        causes source
    
    commit 952dc846e642d06c0fb936bf60f0a2658fbf3bda
    Author: Jorge Rivera <jorge.rivera@one.org>
    Date:   Sat Sep 24 00:40:55 2022 +0200
    
        formatting
    
    commit b5bfe75a530babb8ed9438b77cf10e5913da1bc9
    Author: Jorge Rivera <jorge.rivera@one.org>
    Date:   Fri Sep 23 16:56:30 2022 +0200
    
        inflation
    
    commit 46ef9832f66b99d0b817db47d738bb22e6256d93
    Author: Jorge Rivera <jorge.rivera@one.org>
    Date:   Fri Sep 23 14:50:50 2022 +0200
    
        inflation indicator change

--- scripts/common.py
+++ scripts/common.py
@@ -1,6 +1,56 @@
 """ """
+import country_converter as coco
 import pandas as pd
+from country_converter import country_converter
 
+
+def get_full_africa_iso3() -> list:
+    africa = (
+        coco.CountryConverter()
+        .data[["ISO3", "continent"]]
+        .query("continent == 'Africa'")
+        .ISO3.to_list()
+    )
+
+    # Add sub saharan Africa
+    africa.append("SSA")
+
+    # Add World
+    africa.append("WLD")
+
+    return africa
+
+
+def sort_name_first(
+    df: pd.DataFrame,
+    name: str,
+    name_column: str,
+    date_column: str,
+    keep_current_sorting=True,
+):
+
+    if not keep_current_sorting:
+        df = df.sort_values([date_column, name_column], ascending=[True, False])
+
+    top = df.query(f"{name_column} == '{name}'").reset_index(drop=True)
+    other = df.query(f"{name_column} != '{name}'").reset_index(drop=True)
+
+    return pd.concat([top, other], ignore_index=True)
+
+
+def base_africa_df():
+    """Create a map with geometries for all african countries"""
+
+    return (
+        country_converter.CountryConverter()
+        .data[["ISO3", "continent"]]
+        .rename(columns={"ISO3": "iso_code"})
+        .query("continent == 'Africa'")
+        .drop(columns="continent")
+    )
+
+
+WEO_YEAR: int = 2022
 
 def clean_wb_overview(df: pd.DataFrame) -> pd.DataFrame:
     """Clean World Bank data for overview charts
--- scripts/country_page/site_country_hero.py
+++ scripts/country_page/site_country_hero.py
@@ -7,36 +7,57 @@ from bblocks.dataframe_tools.add import (
     add_gov_exp_share_column,
     add_median_observation,
 )
+from bblocks.cleaning_tools.clean import date_to_str
 from bblocks.import_tools.imf import WorldEconomicOutlook
 from bblocks.import_tools.wfp import WFPData
 from bblocks.import_tools.world_bank import WorldBankData
 from dateutil.relativedelta import relativedelta
 
+from scripts.common import WEO_YEAR
 from scripts.config import PATHS
 from scripts.owid_covid import tools as ot
 
+WEO_INDICATORs = {
+    "NGDP_RPCH": "GDP Growth",
+    "LUR": "Unemployment rate",
+    "GGR_NGDP": "Government Revenue (% GDP)",
+    "GGXWDN_NGDP": "Government Debt (% GDP)",
+}
 
-def _weo_charts() -> None:
+WB_INDICATORs = {
+    "SI.POV.DDAY": "% of population below the poverty line",
+    "SP.POP.TOTL": "Total Population",
+    "SP.DYN.LE00.IN": "Life Expectancy",
+}
+
+
+def __weo_center(df: pd.DataFrame) -> pd.DataFrame:
+    return df.assign(
+        center=lambda d: d.groupby(["iso_code", "indicator"]).value.transform(
+            lambda g: g / g.abs().max()
+        )
+    )
+
+
+def _read_weo() -> pd.DataFrame:
     weo = WorldEconomicOutlook()
 
-    indicators = {
-        "NGDP_RPCH": "GDP Growth",
-        "LUR": "Unemployment rate",
-        "GGR_NGDP": "Government Revenue (% GDP)",
-        "GGXWDN_NGDP": "Government Debt (% GDP)",
-    }
-
-    for c, n in indicators.items():
+    for c, n in WEO_INDICATORs.items():
         weo.load_indicator(indicator_code=c, indicator_name=n)
 
-    df = (
+    return (
         weo.get_data(indicators="all", keep_metadata=True)
         .pipe(add_short_names_column, id_column="iso_code")
         .pipe(filter_african_countries, id_column="iso_code", id_type="ISO3")
-        .loc[lambda d: d.year.dt.year.between(2012, 2024)]
+        .loc[lambda d: d.year.dt.year.between(WEO_YEAR - 10, WEO_YEAR)]
     )
 
-    for indicator in indicators.values():
+
+def _weo_charts() -> None:
+
+    df = _read_weo()
+
+    for indicator in WEO_INDICATORs.values():
         df.loc[df.indicator_name == indicator].filter(
             ["name_short", "indicator_name", "year", "value"], axis=1
         ).to_csv(f"{PATHS.charts}/country_page/overview_{indicator}.csv", index=False)
@@ -46,13 +67,63 @@ def _weo_charts() -> None:
         )
 
 
+def __single_weo_measure(indicator_code: str, comparison_year_difference: int = 1):
+
+    df = _read_weo().pipe(__weo_center)
+
+    data = (
+        df.loc[lambda d: d.indicator == indicator_code]
+        .loc[
+            lambda d: d.year.dt.year.isin(
+                [WEO_YEAR - comparison_year_difference, WEO_YEAR]
+            )
+        ]
+        .assign(year=lambda d: d.year.dt.year)
+        .filter(["name_short", "indicator_name", "year", "value", "center"], axis=1)
+    )
+
+    latest = data.loc[lambda d: d.year == WEO_YEAR]
+    previous = data.loc[
+        lambda d: d.year == WEO_YEAR - comparison_year_difference
+    ].filter(["name_short", "value"], axis=1)
+
+    return (
+        latest.merge(previous, on=["name_short"], suffixes=("", "_previous"))
+        .assign(indicator_name=f"{WEO_YEAR} estimate")
+        .drop("year", axis=1)
+        .assign(lower=f"in {WEO_YEAR - comparison_year_difference}")
+        .filter(
+            [
+                "name_short",
+                "indicator_name",
+                "value",
+                "lower",
+                "value_previous",
+                "center",
+            ],
+            axis=1,
+        )
+    )
+
+
+def _gdp_growth_single_measure() -> None:
+    # GDP Growth
+
+    gdp_growth_chart = __single_weo_measure("NGDP_RPCH", comparison_year_difference=1)
+
+    # chart version
+    gdp_growth_chart.to_csv(
+        f"{PATHS.charts}/country_page/overview_GDP_growth.csv", index=False
+    )
+
+
 def _group_monthly_change(
     group: pd.DataFrame,
     value_columns: list,
     percentage: bool,
     months: int = 1,
 ) -> pd.DataFrame:
-    """""" ""
+
     sdate = group.date.max() - relativedelta(months=months)
     edate = group.date.max()
 
@@ -69,30 +140,78 @@ def _group_monthly_change(
 def _group_interpolate(
     group: pd.DataFrame,
 ) -> pd.DataFrame:
-    """""" ""
     return group.filter(["value"]).interpolate(
         method="linear", limit_direction="forward"
     )
 
 
-def _wfp_charts() -> None:
-    """Data for the Food Security charts on the country pages"""
-
+def _read_wfp() -> WFPData:
     wfp = WFPData()
-
-    source = "World Food Programme HungerMapLive"
 
     for indicator in wfp.available_indicators:
         wfp.load_indicator(indicator)
 
-    inflation = (
+    return wfp
+
+
+def _wfp_inflation(wfp: WFPData) -> pd.DataFrame:
+    return (
         wfp.get_data("inflation")
         .pipe(add_short_names_column, id_column="iso_code")
         .pipe(filter_african_countries, id_type="ISO3")
         .loc[lambda d: d.date.dt.year.between(2018, 2022)]
         .loc[lambda d: d.indicator == "Inflation Rate"]
         .filter(["name_short", "date", "indicator", "value"], axis=1)
+        .rename(
+            columns={
+                "indicator": "indicator_name",
+            }
+        )
     )
+
+
+def _wfp_food_sm(wfp: WFPData) -> pd.DataFrame:
+
+    food = wfp.get_data("insufficient_food")
+
+    food = (
+        food.pipe(add_short_names_column, id_column="iso_code")
+        .pipe(filter_african_countries, id_type="ISO3")
+        .assign(indicator="People with insufficient food consumption")
+        .filter(["name_short", "date", "indicator", "value"], axis=1)
+    )
+
+    change = (
+        food.groupby(["name_short"])
+        .apply(_group_monthly_change, value_columns=["value"], percentage=True)
+        .reset_index(drop=True)
+        .filter(["name_short", "value"], axis=1)
+        .rename(columns={"value": "change"})
+    )
+
+    df = (
+        food.merge(change, on=["name_short"], how="left")
+        .groupby(["name_short"])
+        .last()
+        .reset_index()
+        .assign(
+            date=lambda d: "On " + date_to_str(d.date, "%d %B"),
+            lower="Change in the last month",
+            center=lambda d: d.change / d.change.max(),
+        )
+        .filter(["name_short", "date", "value", "lower", "change", "center"], axis=1)
+    )
+
+    df.to_csv(f"{PATHS.charts}/country_page/overview_food_sm.csv", index=False)
+
+
+def _wfp_charts() -> None:
+    """Data for the Food Security charts on the country pages"""
+
+    source = "World Food Programme HungerMapLive"
+    wfp = _read_wfp()
+
+    inflation = _wfp_inflation(wfp)
 
     # Chart version
     inflation.to_csv(f"{PATHS.charts}/country_page/overview_inflation.csv", index=False)
@@ -101,6 +220,8 @@ def _wfp_charts() -> None:
     inflation.assign(source=source).to_csv(
         f"{PATHS.download}/country_page/overview_inflation.csv", index=False
     )
+
+    _ = _wfp_food_sm(wfp)
 
     food = wfp.get_data("insufficient_food")
 
@@ -175,30 +296,26 @@ def _vax_chart() -> None:
         .reset_index(drop=True)
         .filter(["iso_code", "value"], axis=1)
         .rename(columns={"value": "note"})
-        .assign(note=lambda d: d.note.map("{:+,.1f}pp in the last 3 months".format))
+        .assign(note=lambda d: round(d.note, 3))
     )
 
     vax = (
         vax.pipe(filter_latest_by, date_column="date", value_columns="value")
-        .assign(
-            value=lambda d: d.value.map("{:,.1f}%".format),
-            date=lambda d: d.date.dt.strftime("%d %b %Y"),
-            indicator="Share of population fully vaccinated against COVID-19",
-        )
+        .assign(date=lambda d: "As of " + d.date.dt.strftime("%d %B"))
         .pipe(filter_african_countries, id_type="ISO3")
         .merge(change, on=["iso_code"], how="left")
         .pipe(add_short_names_column, id_column="iso_code", id_type="ISO3")
         .filter(["name_short", "date", "indicator", "value", "note"], axis=1)
         .rename(columns={"date": "As of"})
+        .assign(
+            lower="Change in the previous 3 months",
+            center=lambda d: round(d.note / d.note.max(), 3),
+        )
+        .filter(["name_short", "As of", "value", "lower", "note", "center"], axis=1)
     )
 
     # Chart version
     vax.to_csv(f"{PATHS.charts}/country_page/{chart_name}.csv", index=False)
-
-    # Download version
-    vax.assign(source="Our World In Data").to_csv(
-        f"{PATHS.download}/country_page/{chart_name}.csv", index=False
-    )
 
 
 def _debt_chart() -> None:
@@ -215,13 +332,10 @@ def _debt_chart() -> None:
         .pipe(add_short_names_column, id_column="country_name")
         .loc[lambda d: d.year == 2022]
         .filter(["name_short", "year", "Total"], axis=1)
+        .assign(year=lambda d: d["year"].astype(str) + " estimate")
         .rename(columns={"year": "As of", "Total": "value"})
-        .assign(indicator="Debt Service, Total (USD million)")
-        .filter(["name_short", "As of", "indicator", "value"], axis=1)
-        .assign(
-            value_units=lambda d: d.value * 1e6,
-            value=lambda d: d.value.map("{:,.0f}".format),
-        )
+        .assign(value_units=lambda d: d.value * 1e6)
+        .filter(["name_short", "As of", "indicator", "value", "value_units"], axis=1)
         .pipe(add_iso_codes_column, id_column="name_short", id_type="short_name")
         .pipe(
             add_gov_exp_share_column,
@@ -234,31 +348,21 @@ def _debt_chart() -> None:
         )
         .drop(columns=["value_units", "iso_code"])
         .assign(
-            note=lambda d: d.note.round(1).astype(str) + "% of government expenditure"
+            note=lambda d: d.note.round(1), center="", lower="of government spending"
         )
+        .filter(["name_short", "As of", "value", "lower", "note", "center"], axis=1)
     )
 
     # Chart version
-    debt.to_csv(f"{PATHS.charts}/country_page/overview_debt.csv", index=False)
+    debt.to_csv(f"{PATHS.charts}/country_page/overview_debt_sm.csv", index=False)
 
-    # Download version
-    debt.assign(
-        source="World Bank International Debt Statistics database, 2022"
-    ).to_csv(f"{PATHS.download}/country_page/overview_debt.csv", index=False)
+# ---------- WORLD BANK ------------ #
 
 
-def _basic_info_chart() -> None:
-    """Poverty headcount, total population, life expectancy, and age dependency"""
-
+def _read_wb() -> dict:
     wb = WorldBankData()
 
-    indicators = {
-        "SI.POV.DDAY": "% of population below the poverty line",
-        "SP.POP.TOTL": "Total Population",
-        "SP.DYN.LE00.IN": "Life Expectancy",
-    }
-
-    for code, name in indicators.items():
+    for code, name in WB_INDICATORs.items():
         wb.load_indicator(code, indicator_name=name, most_recent_only=True)
 
     dfs = {}
@@ -267,29 +371,66 @@ def _basic_info_chart() -> None:
             wb.get_data(indicator)
             .pipe(filter_african_countries, id_column="iso_code", id_type="ISO3")
             .pipe(add_short_names_column, id_column="iso_code", id_type="ISO3")
-            .assign(indicator=lambda d: d.indicator_code.map(indicators))
+            .assign(indicator=lambda d: d.indicator_code.map(WB_INDICATORs))
             .filter(["date", "name_short", "indicator", "value"], axis=1)
         )
 
-    poverty = (
-        dfs["SI.POV.DDAY"]
-        .assign(
-            source="World Bank, Poverty and Inequality Platform.",
-            value=lambda d: d.value.map("{:,.1f}%".format),
-            date=lambda d: d.date.dt.year,
+    return dfs
+
+
+def _read_wb_ts() -> dict:
+    wb = WorldBankData()
+
+    for code, name in WB_INDICATORs.items():
+        wb.load_indicator(code, indicator_name=name)
+
+    dfs = {}
+    for indicator in wb.indicators:
+        dfs[indicator] = (
+            wb.get_data(indicator)
+            .pipe(filter_african_countries, id_column="iso_code", id_type="ISO3")
+            .pipe(add_short_names_column, id_column="iso_code", id_type="ISO3")
+            .assign(indicator=lambda d: d.indicator_code.map(WB_INDICATORs))
+            .filter(["date", "name_short", "indicator", "value"], axis=1)
         )
-        .rename(columns={"date": "As of"})
-        .filter(["name_short", "As of", "indicator", "value", "source"], axis=1)
+
+    return dfs
+
+
+def _wb_poverty_single_measure(data_dict: dict) -> pd.DataFrame:
+
+    data_dict = _read_wb_ts()
+
+    df90s = (
+        data_dict["SI.POV.DDAY"]
+        .loc[lambda d: d.date.dt.year.between(1989, 2000)]
+        .dropna(subset=["value"])
+        .drop_duplicates(subset=["name_short"], keep="first")
+    )
+    df_mrnev = (
+        data_dict["SI.POV.DDAY"]
+        .dropna(subset=["value"])
+        .drop_duplicates(subset=["name_short"], keep="last")
     )
 
-    # Chart version
-    poverty.drop("source", axis=1).to_csv(
-        f"{PATHS.charts}/country_page/overview_poverty.csv", index=False
+    return (
+        df_mrnev.merge(
+            df90s, on=["name_short", "indicator"], suffixes=["", "_90s"], how="left"
+        )
+        .assign(
+            name=lambda d: "As of " + d.date.dt.year.astype(str),
+            lower=lambda d: d.date_90s.apply(
+                lambda x: f"In {x.year}"
+                if not pd.isnull(x)
+                else "Comparison not available"
+            ),
+            center=lambda d: (d.value - d.value_90s) / d.value_90s,
+        )
+        .filter(["name_short", "name", "value", "lower", "value_90s", "center"], axis=1)
     )
 
-    # Download version
-    poverty.to_csv(f"{PATHS.download}/country_page/overview_poverty.csv", index=False)
 
+def _wb_population(data_dict: dict) -> pd.DataFrame:
     pop_source = (
         "( 1 ) United Nations Population Division. World Population Prospects:"
         " 2019 Revision. ( 2 ) Census reports and other statistical publications"
@@ -299,9 +440,8 @@ def _basic_info_chart() -> None:
         " Bureau: International Database, and ( 6 ) Secretariat of the Pacific"
         " Community: Statistics and Demography Programme."
     )
-
-    population = (
-        dfs["SP.POP.TOTL"]
+    return (
+        data_dict["SP.POP.TOTL"]
         .assign(
             source=pop_source,
             value=lambda d: d.value.div(1e6).map("{:,.1f} million " "people".format),
@@ -310,16 +450,9 @@ def _basic_info_chart() -> None:
         .rename(columns={"date": "As of"})
         .filter(["name_short", "As of", "indicator", "value", "source"], axis=1)
     )
-    # Chart version
-    population.drop("source", axis=1).to_csv(
-        f"{PATHS.charts}/country_page/overview_population.csv", index=False
-    )
 
-    # Download version
-    population.to_csv(
-        f"{PATHS.download}/country_page/overview_population.csv", index=False
-    )
 
+def _wb_life_exp(data_dict: dict) -> pd.DataFrame:
     life_source = (
         "( 1 ) United Nations Population Division. World Population"
         " Prospects: 2019 Revision, or derived from male and female life "
@@ -331,9 +464,8 @@ def _basic_info_chart() -> None:
         "Database, and ( 6 ) Secretariat of the Pacific Community: Statistics "
         "and Demography Programme."
     )
-
-    life = (
-        dfs["SP.DYN.LE00.IN"]
+    return (
+        data_dict["SP.DYN.LE00.IN"]
         .assign(source=life_source)
         .assign(
             value=lambda d: d.value.map("{:,.1f} years".format),
@@ -343,6 +475,29 @@ def _basic_info_chart() -> None:
         .filter(["name_short", "As of", "indicator", "value", "source"], axis=1)
     )
 
+
+def _basic_info_chart() -> None:
+    """Poverty headcount, total population, life expectancy, and age dependency"""
+
+    dfs = _read_wb()
+
+    poverty = _wb_poverty_single_measure(dfs)
+
+    # Chart version
+    poverty.to_csv(f"{PATHS.charts}/country_page/overview_poverty.csv", index=False)
+
+    population = _wb_population(dfs)
+    # Chart version
+    population.drop("source", axis=1).to_csv(
+        f"{PATHS.charts}/country_page/overview_population.csv", index=False
+    )
+
+    # Download version
+    population.to_csv(
+        f"{PATHS.download}/country_page/overview_population.csv", index=False
+    )
+
+    life = _wb_life_exp(dfs)
     # Chart version
     life.drop("source", axis=1).to_csv(
         f"{PATHS.charts}/country_page/overview_life_expectancy.csv", index=False
@@ -414,6 +569,8 @@ def key_indicators_chart() -> None:
     # Create csvs for the WEO charts
     _weo_charts()
 
+    _gdp_growth_single_measure()
+
     # Create csvs for the WFP charts
     _wfp_charts()
 
@@ -424,7 +581,7 @@ def key_indicators_chart() -> None:
     _debt_chart()
 
     # Create csvs for the Basic Info charts
-    _basic_info_chart()
+    # _basic_info_chart()
 
     # Create CSVs for Food Security charts
     _food_sec_chart()
