89e9f8ab72832f58b200969a25860fe05c59a049
Jorge Rivera <jorge.rivera@one.org>
2022-10-03 13:54:41 +0200

    ODA GNI chart

--- scripts/oda/topic_charts.py
+++ scripts/oda/topic_charts.py
@@ -1,9 +1,8 @@
 import pandas as pd
-from bblocks.cleaning_tools.clean import format_number
-from bblocks.cleaning_tools.filter import filter_latest_by
 from pydeflate import deflate
 
 from scripts.config import PATHS
+from scripts.logger import logger
 from scripts.oda import common
 
 
@@ -17,11 +16,7 @@ def global_aid_ts() -> None:
         common.read_total_oda(official_definition=True)
         .merge(gni, on=["year", "donor_code"], how="left", suffixes=("", "_gni"))
         .pipe(common.append_dac_total)
-        # .pipe(common.add_constant_change_column, base=common.CONSTANT_YEAR)
-        .assign(
-            #    pct_change=lambda d: "Real change from previous year: " + d["pct_change"],
-            oda_gni=lambda d: round(100 * d.value / d.value_gni, 2),
-        )
+        .assign(oda_gni=lambda d: round(100 * d.value / d.value_gni, 2))
         .assign(
             value=lambda d: deflate(
                 d,
@@ -32,7 +27,7 @@ def global_aid_ts() -> None:
                 id_type="DAC",
                 source_col="value",
                 target_col="const",
-            ).const
+            ).const.round(2)
         )
         .pipe(common.add_short_names)
         .assign(
@@ -43,18 +38,21 @@ def global_aid_ts() -> None:
         .rename(columns={"value": "ODA (left-axis)", "oda_gni": "ODA/GNI (right-axis)"})
     )
 
+    df = common.sort_dac_first(df, keep_current_sorting=True)
+
     # chart version
     df.to_csv(f"{PATHS.charts}/oda_topic/oda_gni_ts.csv", index=False)
+    logger.debug("Saved live chart oda_gni_ts.csv")
 
     # download version
     source = "OECD DAC Creditor Reporting System (CRS)"
     df.assign(source=source).to_csv(
         f"{PATHS.download}/oda_topic/oda_gni_ts.csv", index=False
     )
+    logger.debug("Saved download chart oda_gni_ts.csv")
 
 
 def oda_gni_single_year() -> None:
-
     gni = common.read_gni().filter(["year", "donor_code", "value"], axis=1)
 
     df = (
@@ -69,10 +67,10 @@ def oda_gni_single_year() -> None:
         .assign(missing=lambda d: d.missing.apply(lambda v: v if v > 0 else 0))
         .assign(
             value=lambda df_: df_.value.apply(
-                lambda d: f"{d/1e3:.2f} billion" if d > 1e3 else f"{d:.1f} million"
+                lambda d: f"{d / 1e3:.2f} billion" if d > 1e3 else f"{d:.1f} million"
             ),
             missing=lambda df_: df_.missing.apply(
-                lambda d: f"{d/1e3:.2f} billion" if d > 1e3 else f"{d:.1f} million"
+                lambda d: f"{d / 1e3:.2f} billion" if d > 1e3 else f"{d:.1f} million"
             ),
         )
         .loc[lambda d: (d.donor_code != 918) & (d.year >= common.START_YEAR)]
@@ -94,7 +92,6 @@ def oda_gni_single_year() -> None:
 
 
 def _sectors_ts() -> tuple[pd.DataFrame, list]:
-
     df_ = common.read_sectors()
     common.check_sector_completeness(df_=df_)
 
@@ -136,7 +133,6 @@ def _sectors_ts() -> tuple[pd.DataFrame, list]:
 
 
 def sector_totals() -> None:
-
     df, order = _sectors_ts()
 
     df = (
@@ -151,7 +147,6 @@ def sector_totals() -> None:
 
 
 def key_sector_shares() -> None:
-
     key_sectors = [
         "Humanitarian",
         "Education",
