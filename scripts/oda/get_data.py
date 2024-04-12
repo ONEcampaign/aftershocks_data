adc50822e7320d4736ce445af34684f0ac39cf55
Jorge Rivera <jorge.rivera@one.org>
2024-04-12 11:18:13 +0200

    second pass

--- scripts/oda/get_data.py
+++ scripts/oda/get_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from oda_data import set_data_path, ODAData
+from oda_data import set_data_path, ODAData, recipient_groupings, read_crs
 from pydeflate import set_pydeflate_path, update_dac1
 
 from scripts import config
@@ -35,6 +35,7 @@ DAC = [
     69,
     75,
     76,
+    82,
     84,
     301,
     302,
@@ -179,6 +180,113 @@ def get_oda_to_regions():
     data.to_csv(f"{config.PATHS.raw_oda}/total_oda_by_region.csv", index=False)
 
 
+def get_ukraine_bilat() -> pd.DataFrame:
+    recipients = {85: "Ukraine"}
+
+    indicators = {"recipient_bilateral_flow_net": "bilateral"}
+
+    oda = ODAData(
+        years=YEARS,
+        donors=DAC + [20001],
+        recipients=list(recipients),
+        include_names=True,
+        prices="constant",
+        base_year=2022,
+    )
+
+    oda.load_indicator(list(indicators))
+
+    data = oda.get_data()
+
+    return data
+
+
+def get_ukraine_crs() -> pd.DataFrame:
+    df = read_crs(years=range(2015, 2023))
+
+    df = df.loc[lambda d: d.recipient_code == 85].loc[lambda d: d.donor_code.isin(DAC)]
+    df = df.loc[lambda d: d.flow_name != "Other Official Flows (non Export Credit)"]
+
+    df = (
+        df.groupby(
+            [
+                "year",
+                "donor_code",
+                "donor_name",
+                "recipient_name",
+                # "flow_name",
+            ],
+            observed=True,
+            dropna=False,
+        )[["usd_disbursement", "usd_received", "usd_grant_equiv"]]
+        .sum()
+        .reset_index()
+    )
+
+    df["net_disbursement"] = df["usd_disbursement"] - df["usd_received"]
+
+    dac = df.loc[lambda d: d.donor_code != 918]
+    eu = df.loc[lambda d: d.donor_code == 918].drop(
+        columns=["usd_disbursement", "usd_received"]
+    )
+
+    dac = (
+        dac.groupby(["year", "recipient_name"], observed=True, dropna=False)[
+            ["net_disbursement", "usd_grant_equiv"]
+        ]
+        .sum()
+        .reset_index()
+        .assign(donor_name="DAC Countries")
+    )
+
+    return pd.concat([dac], ignore_index=True)
+
+
+def get_idrc() -> pd.DataFrame:
+
+    indicators = {"idrc_ge_linked": "irdc"}
+
+    oda = ODAData(
+        years=YEARS,
+        donors=DAC + [20001],
+        include_names=True,
+        prices="constant",
+        base_year=2022,
+    )
+
+    oda.load_indicator(list(indicators))
+
+    data = oda.get_data()
+
+    return data
+
+
+def get_total_official():
+    oda = ODAData(
+        years=YEARS,
+        donors=DAC + [20001],
+        include_names=True,
+        prices="constant",
+        base_year=2022,
+    )
+    oda.load_indicator(["total_oda_flow_net", "total_oda_ge"])
+
+    flow = (
+        oda.get_data("total_oda_flow_net")
+        .assign(flows_code=1140, indicator="ODA (net flows)")
+        .query("year < 2018")
+    )
+    ge = (
+        oda.get_data("total_oda_ge")
+        .assign(flows_code=1160, indicator="ODA (grant equivalent)")
+        .query("year >= 2018")
+    )
+
+    df = pd.concat([flow, ge], ignore_index=True)
+
+    return df
+
+
 if __name__ == "__main__":
     get_totals()
     get_oda_gni()
@@ -186,3 +294,4 @@ if __name__ == "__main__":
     get_oda_by_income()
     get_oda_to_africa()
     get_oda_to_regions()
+    # df = get_ukraine_crs()
